7efe1db9f33fb48909cda662233a233bbe48b9c6
Md Rasel <mdrasel551219@gmail.com>
2024-11-18 21:23:52 +0600

    add animation

--- java.js
+++ java.js
@@ -1,6 +1,7 @@
 const tl = gsap.timeline();
 
 tl.from(".heading1", {
+    delay: 2,
     opacity: 0,
     duration: 2,
     scale:0,
@@ -8,9 +9,37 @@ tl.from(".heading1", {
 tl.to(".heading1", {
     opacity: 0,
     duration: 2,
-    delay: 1,
-    scale:1,
+    delay: 2,
+scale: 0,
   });
+  
+
+  tl.from(".logo-img",{
+    opacity: 0,
+    duration: 2,
+  })
+  tl.from(".heading2",{
+    x: "-100%",
+    opacity: 0,
+    duration: 1.5,
+    
+
+   
+  })
+  tl.from(".heading3",{
+    x: "200%",
+    duration: 1.5,
+    opacity: 0,
+
+   
+  })
+  tl.to(".main-animation",{
+    opacity: 0,
+    duration: 2,
+    delay:2,
+  
+   
+  })
   tl.to(".animation",{
     opacity: 0,
     duration: 2,
@@ -20,3 +49,4 @@ tl.to(".heading1", {
   tl.to(".animation",{
     y: "-100%",
   })
+  
